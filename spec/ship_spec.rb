41664de66c639e6293efa9b134c5040a89f1f911
Huy Phan <huyphan520@gmail.com>
2022-12-05 21:37:07 -0600

    Ship class finish

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,19 +1,38 @@
 require 'rspec'
 require './lib/ship'
 
-Rspec.describe Ship do
+RSpec.describe Ship do
     it "exists" do
         cruiser = Ship.new("Cruiser", 3)
 
-        expect(ship).to be_an_instance_of(ship)
+        expect(cruiser).to be_an_instance_of(Ship)
     end
         
     it "has attributes" do
         cruiser = Ship.new("Cruiser", 3)
 
-        expect(ship.name).to eq("Cruiser")
-        expect(ship.length).to eq(3)
-        expect(ship.health).to eq(ship.length)
+        expect(cruiser.name).to eq("Cruiser")
+        expect(cruiser.length).to eq(3)
+        expect(cruiser.health).to eq(cruiser.length)
+    end
+
+    it "sunk?" do
+        cruiser = Ship.new("Cruiser", 3)
+
+        expect(cruiser.sunk?).to eq(false)
+
+    end
+
+    it "hit" do
+        cruiser = Ship.new("Cruiser", 3)
+
+        cruiser.hit
+        expect(cruiser.health).to eq(2)
+        cruiser.hit
+        expect(cruiser.health).to eq(1)
+        expect(cruiser.sunk?).to eq(false)
+        cruiser.hit
+        expect(cruiser.sunk?).to eq(true)
     end
 
 end
