517db723d800ab3e7ed5220d9e2f0183e270901a
Nickolas Marquez <nickomarquez7@gmail.com>
2022-12-06 22:27:16 -0700

    Cell classs finish

---
+++ lib/cell.rb
@@ -0,0 +1,53 @@
+class Cell
+
+    attr_reader :coordinate, :ship
+
+    def initialize(coordinate)
+        @coordinate = coordinate
+        @ship = nil
+        @fired_upon = false
+    
+    end
+
+    def empty?
+
+        @ship == nil
+
+    end
+
+    def place_ship(ship)
+
+        @ship = ship
+
+    end
+
+    def fired_upon?
+
+        @fired_upon
+
+    end    
+
+    def fire_upon
+
+        @fired_upon = true
+
+        if @ship != nil
+            @ship.hit
+        end
+
+    end
+
+    def render(show = false)
+
+        
+        if fired_upon? 
+            return "M" if empty?
+            return "X" if ship.sunk?
+            # require "pry"; binding.pry
+            return "H" if !empty?
+        end
+        return "S" if !empty? && show == true
+        "."
+    end
+
+end
