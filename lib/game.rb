8004e5567b9caf57fe8e1f7c6809ec06ee9bccae
Nickolas Marquez <nickomarquez7@gmail.com>
2022-12-11 21:54:15 -0700

    Beginning fire

--- lib/game.rb
+++ lib/game.rb
@@ -33,28 +33,91 @@ class Game
             coordinates = valid_cpu_coordinates(ship)
             @cpu_board.place(ship, coordinates)
         end
+        puts @cpu_board.render(show = true)
     end
 
-    def start 
-        p "Welcome to BATTLESHIP"
-        p "Enter p to play. Enter q to quit."
+    def place_player_cruiser
+        puts "I have laid out my ships on the grid."
+        puts "You now need to lay out your two ships."
+        puts "The Cruiser is three units long and the Submarine is two units long."
+        puts "Enter the squares for the Cruiser (3 spaces):"
+        puts ">"
 
-        user_input = gets.chomp.upcase
+        user_input = gets.chomp.upcase.split 
+
+        until @player_board.valid_placement?(@player_cruiser, user_input) == true 
+            puts "Try again, type in different coordinates"
+            user_input = gets.chomp.upcase.split 
+        end
+        @player_board.place(@player_cruiser, user_input)
+         puts @player_board.render(show = true)
+    end
+
+    def place_player_submarine 
+
+        puts "Enter the squares for the Submarine (2 spaces):"
+        puts ">" 
+
+        user_input = gets.chomp.upcase.split
+
+        until @player_board.valid_placement?(@player_submarine, user_input) == true 
+            puts "Try again, type in different coordinates"
+            user_input = gets.chomp.upcase.split 
+        end
+        @player_board.place(@player_submarine, user_input)
+         puts @player_board.render(show = true)
+    end
+
+    def turn
+        puts "=============COMPUTER BOARD============="
+        puts @cpu_board.render
+        puts "==============PLAYER BOARD=============="
+        puts @player_board.render(show = true)
+    end
+
+    # def player_shot
+
+    #     user_input = gets.chomp.upcase
+    #     @cpu_board.valid_coordinate?(user_input) == true
+    #     @cpu_board.cells[user_input].fire_upon
+    #     if @cpu_board.cells[user_input].empty?
+    #         "M"
+    #         puts "I will get you next time"
+    #     elsif @cpu_board.cells[user_input].ship.sunk?
+    #         "X"
+    #         puts "I destroyed your ship"
+    #     elsif @cpu_board.cells[user_input].!empty?
+    #         "H"
+    #         puts "I got you this time"
+    #     else 
+    #         "."
+    #     end
+
+    # end
+
+    def start 
+        puts "Welcome to BATTLESHIP"
+        puts "Enter p to play. Enter q to quit."
+
+        user_input = gets.chomp.downcase
         if user_input == "p"
             place_cpu_ships
+            place_player_cruiser
+            place_player_submarine
+            turn
+            # player_shot
         else
             p "Come again"
         end
     end
 
-    def turn_action
-        p "I have laid out my ships the grid."
-        p "You now need to lay out your two ships."
-        p "The Cruiser is three units long and the Submarine is two units long."
+  
 
 
 
 
 
-    end
+
+
+
 end
